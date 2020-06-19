dab251ad97368f77247acd239e142acf430ac30f
MajaMatti <30718441+MajaMatti@users.noreply.github.com>
2020-06-19 22:34:17 +0200

    Update gps_lock_led_proto.ino

--- gps_lock_led_proto.ino
+++ gps_lock_led_proto.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <Adafruit_NeoPixel.h>
@@ -15,15 +16,30 @@ int TXPin = 3; //RX auf dem Modul
 int GPSBaud = 9600;
 int LockPin = 8;
 
+int counter = 0;
+
 // GPS VARIABLEN
 
 bool gps1 = false;
 bool gps2 = false;
 bool gps3 = false;
 
+// EEPROM ADDRESS
+
+int addgps1 = 0;
+int addgps2 = 1;
+int addgps3 = 2;
+
+// EEPROM Var
+
+int eeprom_gps1 = 0;
+int eeprom_gps2 = 0;
+int eeprom_gps3 = 0;
+
+int debug = 2;
 
 //Inbetriebnahme Koordinaten:
-
+/*
 float Latitude_1 = 52.489784;
 float Longitude_1 = 13.444000;
 
@@ -32,9 +48,10 @@ float Longitude_2 = Longitude_1;
 
 float Latitude_3  = Latitude_1;
 float Longitude_3 = Longitude_1;
+*/
 
 //LOCATION Coordinates:
-/*
+
 float Latitude_1  = 52.490830;
 float Longitude_1 = 13.445744;
 
@@ -43,10 +60,11 @@ float Longitude_2 = 13.442485;
 
 float Latitude_3  = 52.489395;
 float Longitude_3 = 13.443457;
-*/
 
-float tolWindowLat = 0.0001;
-float tolWindowLng = 0.00015;
+
+
+float tolWindowLat = 0.0003;
+float tolWindowLng = 0.0004;
 
 // LOCATION WINDOW 1:
 
@@ -125,7 +143,20 @@ Serial.println(UntereGrenzeLng_3,6);
 Serial.println("Coord 3 Langitude high:");
 Serial.println(ObereGrenzeLng_3,6);
 Serial.println();
-  
+
+// EEPROM Read
+
+eeprom_gps1 = EEPROM.read(addgps1);
+eeprom_gps2 = EEPROM.read(addgps2);
+eeprom_gps3 = EEPROM.read(addgps3);
+
+pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+pixels.setPixelColor(1, pixels.Color(0, 0, 0));
+pixels.setPixelColor(2, pixels.Color(0, 0, 0));
+
+
+
+
 }
 
 //     /_  _  _ 
@@ -134,12 +165,28 @@ Serial.println();
 
 void loop()
 {
+
+switch (debug) {
+  case 1:
+  
+// EEPROM eraser
+
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
+  }
+  
+    break;
+  case 2:
+
+  //eepromSetup();
   
   // This sketch displays information every time a new sentence is correctly encoded.
   while (gpsSerial.available() > 0)
     if (gps.encode(gpsSerial.read()))
       displayInfo();
-
+ 
+      
+      
   // If 5000 milliseconds pass and there are no characters coming in
   // over the software serial port, show a "No GPS detected" error
   if (millis() > 5000 && gps.charsProcessed() < 10)
@@ -147,6 +194,12 @@ void loop()
     Serial.println("No GPS detected");
     while(true);
   }
+      
+    break;
+  default:
+
+    break;
+}
 }
 
 //      _/. __  /_     /_ _/|_ 
@@ -167,6 +220,9 @@ void displayInfo()
   else
   {
     Serial.println("Location: Not Available");
+
+    
+    
   }
   
   Serial.print("Date: ");
@@ -203,61 +259,112 @@ void displayInfo()
     Serial.println("Not Available");
   }
 
+eepromSetup();
 
+// Check the Values:
 
-
-
-
-if ((UntereGrenzeLat_1 < gps.location.lat())&& (gps.location.lat()< ObereGrenzeLat_1)&&(UntereGrenzeLng_1 < gps.location.lng())&&(gps.location.lng() < ObereGrenzeLng_1)){
+  if ((UntereGrenzeLat_1 < gps.location.lat())&& (gps.location.lat()< ObereGrenzeLat_1)&&(UntereGrenzeLng_1 < gps.location.lng())&&(gps.location.lng() < ObereGrenzeLng_1)){
     
     gps1 = true;
+    EEPROM.write(addgps1, gps1);
     Serial.println(gps.location.lng());
     Serial.println(gps.location.lat());
     Serial.println(gps1);
-    }
+  }
     
-if ((UntereGrenzeLat_2 < gps.location.lat())&& (gps.location.lat()< ObereGrenzeLat_2)&&(UntereGrenzeLng_2 < gps.location.lng())&&(gps.location.lng() < ObereGrenzeLng_2)){
-    
+  if ((UntereGrenzeLat_2 < gps.location.lat())&& (gps.location.lat()< ObereGrenzeLat_2)&&(UntereGrenzeLng_2 < gps.location.lng())&&(gps.location.lng() < ObereGrenzeLng_2)){
+  
     gps2 = true;
+    EEPROM.write(addgps2, gps2);
     Serial.println(gps.location.lng());
     Serial.println(gps.location.lat());
     Serial.println(gps2);
-    }
+  }
     
-if ((UntereGrenzeLat_3 < gps.location.lat())&& (gps.location.lat()< ObereGrenzeLat_3)&&(UntereGrenzeLng_3 < gps.location.lng())&&(gps.location.lng() < ObereGrenzeLng_3)){
+  if ((UntereGrenzeLat_3 < gps.location.lat())&& (gps.location.lat()< ObereGrenzeLat_3)&&(UntereGrenzeLng_3 < gps.location.lng())&&(gps.location.lng() < ObereGrenzeLng_3)){
     
     gps3 = true;
+    EEPROM.write(addgps3, gps3);
     Serial.println(gps.location.lng());
     Serial.println(gps.location.lat());
     Serial.println(gps3);
-    }
+  }
+
 
 // LED CONTROL...
-//
-   if (gps1 == true) {
-   pixels.setPixelColor(0, pixels.Color(150, 0, 0));
-   pixels.show();
+
+  if (gps1 == true) {
+    pixels.setPixelColor(0, pixels.Color(150, 0, 0));
+    pixels.show();
   } 
-  
-   if(gps2 == true) {
-   pixels.setPixelColor(1, pixels.Color(0, 150, 0));
-   pixels.show();
+  else
+  {
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
+  }
+  if(gps2 == true) {
+
+     pixels.setPixelColor(1, pixels.Color(0, 150, 0));
+     pixels.show();
+  }
+  else
+  {
+    pixels.setPixelColor(1, pixels.Color(0, 0, 0));
+    pixels.show();
   }
   if(gps3 == true) {
-   pixels.setPixelColor(2, pixels.Color(0, 0, 150));
-   pixels.show();
+
+     pixels.setPixelColor(2, pixels.Color(0, 0, 150));
+     pixels.show();
+  }
+  else
+  {
+    pixels.setPixelColor(2, pixels.Color(0, 0, 0));
+    pixels.show();
   }
 
 
 //  UNLOCK THE LOCK....
-//  
-if((gps1 == true) && (gps2 == true) && (gps3 == true)){
+
+  if((gps1 == true) && (gps2 == true) && (gps3 == true)){
   digitalWrite(LockPin, HIGH);   //open the Lock
   }
-//
+
   
   Serial.println();
   Serial.println();
   delay(1000);
   
 }
+
+
+
+void eepromSetup()
+{
+    // Check the EEPROM values:
+
+    // if there is a EEPROM value equal true then  set the corresponding LED RGB to its color.
+    if (eeprom_gps1 == 1) {
+        gps1 = true;
+    }
+    if (eeprom_gps2 == 1) {
+        gps2 = true;
+    }
+    if (eeprom_gps3 == 1) {
+        gps3 = true;
+    }
+    if (gps1 == true) {
+        pixels.setPixelColor(0, pixels.Color(150, 0, 0));
+        pixels.show();
+    } 
+    if(gps2 == true) {
+        pixels.setPixelColor(1, pixels.Color(0, 150, 0));
+        pixels.show();
+    }
+    if(gps3 == true) {
+        pixels.setPixelColor(2, pixels.Color(0, 0, 150));
+        pixels.show();
+    }
+}
+
+
